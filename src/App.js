24a4938e1269337953272c4ff8083accaa22a5c3
tahmidmahdi <tahmid5550@gmail.com>
2021-02-24 23:49:29 +0600

    images random is generated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+
 
 function App() {
+  const [users, setUsers] = useState([])
+  // console.log('calling from', users)
+  useEffect(() => {
+    fetch(`https://randomuser.me/api/?results=100`)
+    .then(res => res.json())
+    .then(data =>  setUsers(data.results))
+  },[]);
+  console.log('calling log', users);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      {
+        users.map(user => <User name = {user}></User>)
+      }
     </div>
   );
+}
+
+function User(props) {
+  const {title, first, last} = props.name.name;
+  // const {picture} = props.picture.large;
+  // console.log('Img',props.name.picture);
+  // const {picture} = props.name.picture;
+  const url =props.name.picture.large;
+  console.log(url);
+  return(
+    <div>
+      <h3>{title} {first} {last}</h3>
+      <img src={url} alt=""/>
+    </div>
+  )
 }
 
 export default App;
