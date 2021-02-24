2cc85cd067d438a47a377318b5337ce337b62d7b
tahmidmahdi <tahmid5550@gmail.com>
2021-02-25 00:22:17 +0600

    added fitures

--- src/App.js
+++ src/App.js
@@ -7,34 +7,56 @@ function App() {
   const [users, setUsers] = useState([])
   // console.log('calling from', users)
   useEffect(() => {
-    fetch(`https://randomuser.me/api/?results=100`)
-    .then(res => res.json())
-    .then(data =>  setUsers(data.results))
-  },[]);
+    fetch(`https://randomuser.me/api/?results=1000`)
+      .then(res => res.json())
+      .then(data => setUsers(data.results))
+  }, []);
   console.log('calling log', users);
   return (
     <div className="App">
+      <h1>Random User </h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {
+          users.map(user => <User name={user}></User>)
+        }
       </header>
-      {
-        users.map(user => <User name = {user}></User>)
-      }
+
     </div>
   );
 }
 
 function User(props) {
-  const {title, first, last} = props.name.name;
+
+  // decorating img
+  const imgStyle = {
+    borderRadius: '50%',
+  };
+  //decorating div
+  const divStyle = {
+    // marginLeft: '700px',
+    border: '2px solid gray',
+    height: '450px',
+    width: '450px',
+    borderRadius: '20px',
+    margin: "20px"
+  }
+
+
+
+  const { title, first, last } = props.name.name;
   // const {picture} = props.picture.large;
   // console.log('Img',props.name.picture);
   // const {picture} = props.name.picture;
-  const url =props.name.picture.large;
-  console.log(url);
-  return(
-    <div>
+  const url = props.name.picture.large;
+  // console.log(url);
+  return (
+    <div style={divStyle}>
       <h3>{title} {first} {last}</h3>
-      <img src={url} alt=""/>
+      <img style={imgStyle} src={url} alt="" />
+      <p>{props.name.cell}</p>
+      <p>Age: {props.name.dob.age}</p>
+      <p>{props.name.location.country}</p>
     </div>
   )
 }
